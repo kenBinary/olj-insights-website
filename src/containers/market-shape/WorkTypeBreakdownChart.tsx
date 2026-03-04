96b6a3d0ed528202ddd96893a643d060d128a7cc
kenneth <joshuabecaro@gmail.com>
2026-03-04 22:15:35 +0800

    feat: add workt type breakdown chart

---
+++ src/containers/market-shape/WorkTypeBreakdownChart.tsx
@@ -0,0 +1,34 @@
+import Plot from "react-plotly.js";
+import chartJson from "../../data/market-shape/work_type_distribution.json";
+import type { Data, Layout } from "plotly.js";
+import { ChartHeader } from "../../components/ChartHeader";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const chartData: Data[] = (chartJson as any).data;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const chartLayout: Partial<Layout> = (chartJson as any).layout;
+
+export function WorkTypeBreakdownChart() {
+  return (
+    <div className="p-5 border flex-1">
+      <ChartHeader
+        overLine="Composition"
+        title="Work Type Breakdown"
+        description="Distribution of jobs across different employment types (Full-time, Part-time, etc.)."
+      />
+
+      <Plot
+        data={chartData}
+        layout={{
+          ...chartLayout,
+          autosize: true,
+          paper_bgcolor: "transparent",
+          plot_bgcolor: "transparent",
+          height: 400,
+        }}
+        config={{ responsive: true }}
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
+}
