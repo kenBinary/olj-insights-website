211974fe4efa78c422e7c75d1061dbfaf89e907e
kenneth <joshuabecaro@gmail.com>
2026-03-04 22:31:35 +0800

    feat: update App component layout with sections and charts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { MarketDemand } from "./containers/market-demand/MarketDemand";
+import { SalaryLandscape } from "./containers/salary-landscape/SalaryLandscape";
+import { MarketShape } from "./containers/market-shape/MarketShape";
+import { PayVsHours } from "./containers/pay-vs-hours/PayVsHours";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <header className="mx-40 my-10">
+        <p className="text-sm">Philippine Remote Job Market · Data Report</p>
+        <h1 className="text-7xl font-extrabold">
+          The <i> Remote</i> <br />
+          Job Market <br />
+          Decoded.
+        </h1>
+        <p className="w-[50ch] my-2">
+          25, 000+ listings scraped and analyzed to reveal where opportunity
+          lives, what skills are in demand, and how to navigate the remote job
+          market in the Philippines.
+        </p>
+      </header>
+      <main className="mx-40 my-5">
+        <SalaryLandscape />
+        <MarketDemand />
+        <MarketShape />
+        <PayVsHours />
+      </main>
     </>
   );
 }
