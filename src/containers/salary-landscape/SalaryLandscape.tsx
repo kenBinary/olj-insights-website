43ee168a02649c2203eda42bdf2d298384b50bda
kenneth <joshuabecaro@gmail.com>
2026-03-04 19:33:59 +0800

    feat: replace static header with PanelHeader component in SalaryLandscape

--- src/containers/salary-landscape/SalaryLandscape.tsx
+++ src/containers/salary-landscape/SalaryLandscape.tsx
@@ -1,3 +1,4 @@
+import { PanelHeader } from "../../components/PanelHeader";
 import { AveragePayByCategoryChart } from "./AveragePayByCategoryChart";
 import { AveragePayBySubCategoryChart } from "./AveragePayBySubCategoryChart";
 import { PayRangeChart } from "./PayRangeChart";
@@ -6,10 +7,7 @@ import { SalaryDistributionChart } from "./SalaryDistributionChart";
 export function SalaryLandscape() {
   return (
     <section className="py-5">
-      <div className="border-b pb-5 mb-5">
-        <p className="text-sm tracking-wide">01 · Compensation</p>
-        <h2 className="text-4xl font-bold">Salary Landscape</h2>
-      </div>
+      <PanelHeader overLine="01 · Compensation" title="Salary Landscape" />
 
       <div className="flex flex-col gap-1">
         <div className="flex flex-nowrap gap-1">
